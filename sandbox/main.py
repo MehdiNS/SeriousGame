265fe91c202e0ce59caa317d6dde046d8b77f543
mehdi <mehdi@mehdi-W310CZ-CZ-T>
2014-04-10 01:22:45 +0200

    Popup with video working in the sandbox

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,6 +1,7 @@
 from core.window import Window
 import kivy
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.clock import ClockEvent
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
@@ -9,23 +10,17 @@ from kivy.uix.layout import Layout
 from kivy.uix.popup import Popup
 from kivy.uix.video import Video
 from kivy.uix.videoplayer import VideoPlayer
-from kivy.clock import Clock
+from kivy.uix.widget import Widget
 from uix.button import Button
-
-from gameMenu import GameMenuApp
 
 
 kivy.require('1.8.0') 
 
-class Test(Layout):
+class Test(Widget):
     button_box = ObjectProperty()
-    
-    def update(self,dt):
-        pass
-    
-    def playVideo(self, chaine):
+    def playVideo(self):
         layout = BoxLayout(orientation='vertical')
-        video = Video(source=chaine, state='play', size_hint=(1, 0.9));
+        video = Video(source='../video/Img/P2_1.mp4', state='play', size_hint=(1, 0.9));
         layout.add_widget(video);
         button = Button(text='Revenir au jeu', size_hint=(1, 0.1))
         layout.add_widget(button)
@@ -41,8 +36,7 @@ class Test(Layout):
 
 class Main(App):
     def build(self):
-        test = Test()
-        Clock.schedule_interval(test.update, 10.)
-        return Test();
+        return Test().playVideo();
+
 
 Main().run()
